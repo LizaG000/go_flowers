eb662868955f2f46b7b39a90fcd8ee5bcdcc3929
LizaG000 <elizaveta.vladimirovna.g@gmail.com>
2026-06-23 21:46:54 +0300

    добавлены таблица idempotency и правлена таблица избранного

---
+++ migrations/20260623093621_add_idempotency_keys.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE idempotency_keys (
+    key UUID PRIMARY KEY,
+    status VARCHAR(20) NOT NULL,
+    response_code INT,
+    response_body JSONB,
+    payload_hash VARCHAR(64) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS favorites;
